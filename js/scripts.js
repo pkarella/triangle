d9ecf91b656f625c41502503b73f3b4ba3122beb
Ben Metzger and Peter Karellas <peter@>
2017-03-23 14:06:46 -0700

    add paranthesis around comparison operators

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,25 @@
 var triangleCheck = function(sideOne, sideTwo, sideThree) {
   debugger;
-  if (sideOne === sideTwo && sideOne === sideThree) {
+  if ((sideOne === sideTwo) && (sideOne === sideThree)) {
     alert("Equilateral: All sides are equal.")
-  } 
-  else if (sideOne + sideTwo <= sideThree || sideOne + sideThree <= sideTwo || sideTwo + sideThree <= sideOne) {
-      alert("NOT a triangle: The sum of the lengths of any two sides of a triangle is less than or equal to the length of the third side.") }
-  } else if (sideOne === sideTwo && sideOne !== sideThree || sideOne === sideThree && sideOne !== sideTwo || sideTwo === sideThree && sideOne !== sideTwo) {
-      alert("Isosceles: Exactly 2 sides are equal.")
-  } else if (sideOne !== sideTwo && sideOne !== sideThree && sideTwo !== sideThree) {
-      alert("Scalene: No sides are equal.")
+  }
+  else if ((sideOne + sideTwo <= sideThree) || (sideOne + sideThree <= sideTwo) || (sideTwo + sideThree <= sideOne)) {
+    alert("NOT a triangle: The sum of the lengths of any two sides of a triangle is less than or equal to the length of the third side.")
+  }
+  else if (((sideOne === sideTwo) && (sideOne !== sideThree)) || ((sideOne === sideThree) && (sideOne !== sideTwo)) || ((sideTwo === sideThree) && (sideOne !== sideTwo))) {
+    alert("Isosceles: Exactly 2 sides are equal.")
+  }
+  else if ((sideOne !== sideTwo) && (sideOne !== sideThree) && (sideTwo !== sideThree)) {
+    alert("Scalene: No sides are equal.")
   }
 };
 
-$(document).ready(
-  function() {
-    $("#some-form").submit(function(event) {
-      event.preventDefault();
-      var inputOne = parseInt($("input#side-one").val());
-      var inputTwo = parseInt($("input#side-two").val());
-      var inputThree = parseInt($("input#side-three").val());
-      triangleCheck(inputOne, inputTwo, inputThree);
-    });
-  }
-);
+$(document).ready(function() {
+  $("#some-form").submit(function(event) {
+    event.preventDefault();
+    var inputOne = parseInt($("input#side-one").val());
+    var inputTwo = parseInt($("input#side-two").val());
+    var inputThree = parseInt($("input#side-three").val());
+    triangleCheck(inputOne, inputTwo, inputThree);
+  });
+});
